05d3a7674d6270aded74955d18afa205b15391db
Mustafa Samancioglu <msamancioglu@hotmail.com>
2020-07-10 09:38:56 +0300

    Added Elasticsearch / Python Signal integration

--- testapp/models.py
+++ testapp/models.py
@@ -1,7 +1,32 @@
 from django.db import models
-
+from django.db.models import signals
+from datetime import datetime
+from elasticsearch import Elasticsearch
+from django.core import serializers
+import inspect
+import json
 # Create your models here.
 
+def create_animal(sender, instance, created, **kwargs):
+    es = Elasticsearch()
+    data = serializers.serialize('json', (instance, ))
+    caller_function = inspect.stack()[0][3]
+    doc = {
+        'sender': 'Animal',
+        'instance': data,
+        'receiver': caller_function,
+        'timestamp': datetime.now(),
+    }
+
+    print(doc)
+
+    res = es.index(index="log-index", body=doc)
+    print(res['result'])
+    #print(instance.__dict__)
+    #print(instance)
+    #print(sender.__name__)
+    #print(kwargs)
+    print("an animal created!!!!")
 
 class Animal(models.Model):
     name = models.CharField(max_length=100)
@@ -13,4 +38,4 @@ class Animal(models.Model):
     def __str__(self):
         return "{} {}".format(self.name, self.sound)
 
-
+signals.post_save.connect(receiver=create_animal, sender=Animal)
