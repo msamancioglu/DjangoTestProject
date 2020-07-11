4b0ebf818c38dfc5e9745352de4ea8dd920ce2c6
Mustafa Samancioglu <msamancioglu@hotmail.com>
2020-07-11 14:09:05 +0300

    Added nested object type for elasticsearch save log

--- testapp/models.py
+++ testapp/models.py
@@ -3,30 +3,12 @@ from django.db.models import signals
 from datetime import datetime
 from elasticsearch import Elasticsearch
 from django.core import serializers
+from django.forms.models import model_to_dict
+
+
 import inspect
 import json
 # Create your models here.
-
-def create_animal(sender, instance, created, **kwargs):
-    es = Elasticsearch()
-    data = serializers.serialize('json', (instance, ))
-    caller_function = inspect.stack()[0][3]
-    doc = {
-        'sender': 'Animal',
-        'instance': data,
-        'receiver': caller_function,
-        'timestamp': datetime.now(),
-    }
-
-    print(doc)
-
-    res = es.index(index="log-index", body=doc)
-    print(res['result'])
-    #print(instance.__dict__)
-    #print(instance)
-    #print(sender.__name__)
-    #print(kwargs)
-    print("an animal created!!!!")
 
 class Animal(models.Model):
     name = models.CharField(max_length=100)
@@ -37,5 +19,24 @@ class Animal(models.Model):
 
     def __str__(self):
         return "{} {}".format(self.name, self.sound)
+
+def create_animal(sender, instance, created, **kwargs):
+    es = Elasticsearch()
+    data = serializers.serialize('json', [instance, ])
+    #struct = json.loads(data)
+    #data = json.dumps(struct[0])
+    caller_function = inspect.stack()[0][3]
+    import ast
+    doc = {
+        'sender':  type(instance).__name__,
+        'instance': ast.literal_eval(data),
+        'receiver': caller_function,
+        'timestamp': datetime.now(),
+    }
+    print(doc)
+    res = es.index(index="log-index", doc_type="logs",  body=doc)
+    print(res['result'])
+    print(ast.literal_eval(data))
+    print("an animal created!!!!")
 
 signals.post_save.connect(receiver=create_animal, sender=Animal)
